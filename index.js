36baf3aac8cc22899301f581056f2c2cfa15d51d
Doredla-Charan-Sai <146411276+Doredla-Charan-Sai@users.noreply.github.com>
2023-10-04 14:37:58 +0530

    rewrite the columns of book table in sql query

--- index.js
+++ index.js
@@ -64,17 +64,7 @@ app.post("/books/", async (request, response) => {
     price,
     onlineStores,
   } = bookDetails;
-  const addBookQuery = `INSERT INTO book (title,
-    authorId,
-    rating,
-    ratingCount,
-    reviewCount,
-    description,
-    pages,
-    dateOfPublication,
-    editionLanguage,
-    price,
-    onlineStores) VALUES (
+  const addBookQuery = `INSERT INTO book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores) VALUES (
         '${title}',
          ${authorId},
          ${rating},
